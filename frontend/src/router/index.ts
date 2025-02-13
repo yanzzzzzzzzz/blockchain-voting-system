39d5dcdc7187d3e00027143f2ace45dd2f32e8ca
Jack Tsai <jtsai@nexify.com.hk>
2025-02-13 18:00:09 +0800

    feat: add contract check in router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import VotePage from '@/views/votePage.vue'
 import Home from '@/views/Home.vue'
+import { getVotingSystemContract, setupWeb3 } from '@/utils/web3'
 const routes = [
   {
     path: '',
@@ -17,6 +18,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory('/'),
   routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+  try {
+    if (!getVotingSystemContract()) {
+      await setupWeb3()
+    }
+    next()
+  } catch (error) {
+    console.error('Error setting up web3', error)
+    next({ name: 'Home' })
+  }
 })
 
 export default router
